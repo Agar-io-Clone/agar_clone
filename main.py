e035aa646f9e6f259d36a1a2c290687083c3a36c
DaviZCodes <52458432+DaviZCodes@users.noreply.github.com>
2022-10-12 23:29:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ Authors: DaviZCodes and HenryMal
 import random
 import math
 
+class MainCell:
+  def __init__(self, cell_name, cell_size = 500, cell_color):
+    self.cell_name = cell_name
+    self.cell_size = cell_size
+    self.cell_color = cell_color
