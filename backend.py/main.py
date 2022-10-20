bc1e2bfd630bb8c3475e79d1bacd7ac68e6de29d
DaviZCodes <52458432+DaviZCodes@users.noreply.github.com>
2022-10-20 17:24:23 -0400

    Update main.py

--- backend.py/main.py
+++ backend.py/main.py
@@ -4,6 +4,10 @@ Making sure the code runs
 
 import game_file
 
+# enter the command to run the game
+
 if __name__ == "__main__":
-  game_file 
-  # enter the command to run the game
+  game = AgarClone() 
+  
+  game.run()
+
