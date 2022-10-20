b3f31bd2696e40501c84950ef141d667d2ec7c8c
DaviZCodes <52458432+DaviZCodes@users.noreply.github.com>
2022-10-20 17:07:55 -0400

    Update game_file.py

--- backend/game_file.py
+++ backend/game_file.py
@@ -4,19 +4,36 @@ Main function for the agar.io clone
 Authors: DaviZCodes and HenryMal
 '''
 import random
-import math
-import pygame
+import math import sqrt, pi
+import pygame as pyg
+import os
+
+# Display dimensions
+WIDTH = 1920
+HEIGHT = 1080 
+DISPLAY_TUPLE = (WIDTH, HEIGHT)
+
+# Basic ball dimensions
+START_PLAYER_RADIUS = 30
+START_PELLET_RADIUS = 3
 
 # data sets for players and balls
 player = {} # dictionary
 
+# The map
 class Background:
-  def __init__(self,
+  def __init__(self,)
   
-class MainCell:
+# The player
+'''class MainCell:
   def __init__(self, cell_name, cell_size = 500, cell_color):
     self.cell_name = cell_name
     self.cell_size = cell_size
-    self.cell_color = cell_color
+    self.cell_color = cell_color'''
 
+               
+# final class for game
+class AgarClone():
+  def __init__(self):
+    self.display = pyg.display.set_mode(DISPLAY_TUPLE)
                
