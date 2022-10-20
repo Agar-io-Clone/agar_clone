ad5daeb1814d899d0443c58d863c3e9c0d676977
DaviZCodes <52458432+DaviZCodes@users.noreply.github.com>
2022-10-20 17:00:10 -0400

    Update and rename main.py to game_file.py

--- game_file.py
+++ game_file.py
@@ -5,9 +5,18 @@ Authors: DaviZCodes and HenryMal
 '''
 import random
 import math
+import pygame
 
+# data sets for players and balls
+player = {} # dictionary
+
+class Background:
+  def __init__(self,
+  
 class MainCell:
   def __init__(self, cell_name, cell_size = 500, cell_color):
     self.cell_name = cell_name
     self.cell_size = cell_size
     self.cell_color = cell_color
+
+               
